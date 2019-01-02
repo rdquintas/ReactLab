a0c3ec6668c7cd58b2fb350dccb308dcebb4a75d
Ricardo Quintas <rqu@gavdi.com>
2019-01-02 08:01:51 +0000

    events and functions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,24 +3,36 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200?random"
-  };
-
-  styles = {
-    fontSize: 50,
-    fontWeight: "bold"
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge  m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
   }
 
   formatCount() {
