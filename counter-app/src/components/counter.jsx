85e753dc8cd1f56837cfd7119a358fab0a8120cd
Ricardo Quintas <rqu@gavdi.com>
2019-01-26 19:28:38 +0000

    working on counter-app

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
+    // tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
+    // console.log("props", this.props);
+
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -16,28 +18,28 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <ul>
+        {/* <ul>
           {this.state.tags.map(tag => (
             <li key={tag}> {tag} </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     );
   }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClasses() {
     let classes = "badge  m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
