2c681e324983fbd805e0663fcdd75fb855dc6f8c
Ricardo Quintas <rqu@gavdi.com>
2019-01-27 19:28:33 +0000

    fixing SORT on TableHeader

--- vidly/src/components/common/tableHeader.jsx
+++ vidly/src/components/common/tableHeader.jsx
@@ -14,16 +14,27 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = column => {
+    const { sortColumn } = this.props;
+    if (column.path !== this.props.sortColumn.path) return null;
+
+    if (sortColumn.order === "asc") return <i className="m-2 fa fa-sort-asc" />;
+
+    return <i className="m-2 fa fa-sort-desc" />;
+  };
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map(column => (
             <th
+              className="clickable"
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
               {column.label}
+              {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
