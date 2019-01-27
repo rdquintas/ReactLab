34d6700ad1dc5e7c20803077b100514ee0183aac
Ricardo Quintas <rqu@gavdi.com>
2019-01-27 13:34:27 +0000

    adding prop-types

--- vidly/src/components/common/pagination.jsx
+++ vidly/src/components/common/pagination.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
 
 const Pagination = props => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
@@ -26,4 +27,10 @@ const Pagination = props => {
   );
 };
 
+Pagination.PropTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
 export default Pagination;
