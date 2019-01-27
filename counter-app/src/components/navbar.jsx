44bfc1f65f20e34071152ac77ba720883e764de3
Ricardo Quintas <rqu@gavdi.com>
2019-01-27 11:14:35 +0000

    hook methods

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-ligth bg-ligth">
-        <a href="www.google.com" className="navbar-brand">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+
+// Stateless Functional Component
+const NavBar = props => {
+  // class NavBar extends Component {
+  // render() {
+  return (
+    <nav className="navbar navbar-ligth bg-ligth">
+      <a href="www.google.com" className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+// }
 
 export default NavBar;
